f6a3b9964634bfd52a0ae9f322637c621ada7429
Donny Cosby <donnycosby@gmail.com>
2015-07-15 15:58:16 -0400

    Add beer form options

--- both/accounts/config.js
+++ both/accounts/config.js
@@ -1,8 +1,3 @@
 AccountsTemplates.configureRoute('signIn', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('signUp', {layoutTemplate: 'appLayout'});
 AccountsTemplates.configureRoute('ensureSignedIn', {layoutTemplate: 'appLayout'});
-
-
-Accounts.ui.config({
-    UsernameOnly: true,
-});
